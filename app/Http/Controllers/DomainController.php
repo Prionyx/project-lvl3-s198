1f86b7c90efa2ffa8a19affdde67a3f60b7671ce
prionyx <spirifex@gmail.com>
2018-04-07 22:34:41 +0400

    step 6

--- app/Http/Controllers/DomainController.php
+++ app/Http/Controllers/DomainController.php
@@ -18,30 +18,13 @@ class DomainController extends Controller
     public function showNew($id)
     {
         $domain = DB::table('domains')->where('id', $id)->first();
+        
         return view('domain', ['domain' => $domain]);
     }
 
     public function showAll()
     {
         $domains = DB::table('domains')->paginate(5);
-        $client = new Client();
-
-        $url = 'http://laravel.su/';
-        $res = $client->request('GET', $url);
-        $code = $res->getStatusCode();
-        $contentLength = !empty($res->getHeader('Content-Length')) ? $res->getHeader('Content-Length')[0] : 0;
-        $body = ($res->getBody()->getContents());
-
-        $document = new Document($body);
-        $keywords = ($document->first('meta[name=keywords]'));
-        $keywordsContent = $keywords ? $keywords->getAttribute('content') : null;
-        $description = ($document->first('meta[name=description]'));
-        $descriptionContent = $description ? $description->getAttribute('content') : null;
-        $h1 = $document->first('h1');
-        $h1Content = $h1 ? $h1->text() : null;
-        var_dump($h1Content);
-        var_dump($keywordsContent);
-        var_dump($descriptionContent);
 
         return view('domains', ['domains' => $domains]);
     }
